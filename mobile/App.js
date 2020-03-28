14e25871a4857630a5779a6038b2e9719b72ee2f
Marcelo <carvalhomar@gmail.com>
2020-03-28 17:35:22 -0300

    included mobile folder

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,12 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 import React from 'react';
-import { View } from 'react-native';
 
 import Routes from './src/pages/routes';
 
 export default function App() {
   return (
-    <View />
+    <Routes />
      
   );
 }
